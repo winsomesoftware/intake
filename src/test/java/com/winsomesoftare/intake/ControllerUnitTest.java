01063bf6610c94f791ac9f4baefffa5eb1090308
Dale Applegate <dale.applegate@perficient.com>
2022-09-07 10:39:49 -0500

    added testing

---
+++ src/test/java/com/winsomesoftare/intake/ControllerUnitTest.java
@@ -0,0 +1,20 @@
+package com.winsomesoftare.intake;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+public @interface ControllerUnitTest {
+}
